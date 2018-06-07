6672f4b11a5240fa0f544290143a8e5724f1890b
BONGVD <bongvd@gmail.com>
2018-06-07 15:35:07 +0700

    no message

---
+++ Assets/Demo/DemoIAP.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoIAP : MonoBehaviour
+{
+    public GameObject requestPanel;
+    public GameObject itemTemplate;
+    private string[] itemCodes = { "gamercoinpack", "supercoinpack" };
+    private Grd.GrdItem[] items;
+    private Grd.GrdPurchaseRequest request;
+    // Use this for initialization
+    void Start()
+    {
+        requestPanel.gameObject.SetActive(false);
+        itemTemplate.gameObject.SetActive(false);
+        Grd.GrdManager.Init("a9d42958088fd0cbc093378285852b8b71f3f45e", "8c6744c9d42ec2cb9e8885b54ff744d094f6d7e04a4d452035300f18b984988cc52f1bd66cc19d05628bd8bf27af3ad61d13fd714139ea639d44f41fb13ebf65", Grd.GrdNet.Test);
+        LoadItemsInfo();
+    }
+    private IEnumerator DownLoadIcon(string url, Image img)
+    {
+        if (url != null && url.Length > 0)
+        {
+            WWW w = new WWW(url);
+            yield return w;
+            if (w.texture != null)
+            {
+                img.sprite = Sprite.Create(w.texture, new Rect(0, 0, (int)w.texture.width, (int)w.texture.height), new Vector2(0.5f, 0.5f));
+            }
+        }
+        else
+        {
+            img.gameObject.SetActive(false);
+        }
+    }
+    public void OnCloseRequestPanel()
+    {
+        requestPanel.SetActive(false);
+    }
+    private void ShowBuy(int itemIndex)
+    {
+        Grd.GrdManager.RequestBuyItem(items[itemIndex].itemcode, 1, (error, args) =>
+        {
+            if (error == 0)
+            {
+                allowChecking = true;
+                request = args.Data;
+                requestPanel.SetActive(true);
+                requestPanel.transform.GetChild(0).Find("ItemName").GetComponent<Text>().text = items[itemIndex].itemname;
+                requestPanel.transform.GetChild(0).Find("Price").GetComponent<Text>().text = items[itemIndex].price.ToString();
+                requestPanel.transform.GetChild(0).Find("RequestStatus").GetComponent<Text>().text = "Waiting for purchase...";
+                Grd.GrdManager.GetRequestPurchaseQRCode(request.RequestId, (err1, args1) =>
+                {
+                    if (err1 == 0)
+                    {
+                        var qrcode = args1.Data;
+                        requestPanel.transform.GetChild(0).Find("QRCode").GetComponent<Image>().sprite = Sprite.Create(qrcode, new Rect(0, 0, qrcode.width, qrcode.height), new Vector2(0.5f, 0.5f));
+                    }
+                });
+                StartCoroutine(DownLoadIcon(items[itemIndex].itemicon, requestPanel.transform.GetChild(0).Find("ItemIcon").GetComponent<Image>()));
+            }
+        });
+    }
+    private void LoadItemsInfo()
+    {
+        Grd.GrdManager.GetItemsInfo(itemCodes, (error, args) =>
+        {
+            if (error == 0)
+            {
+                items = args.Data;
+                for (int i = 0; i < args.Data.Length; i++)
+                {
+                    GameObject item = Instantiate(itemTemplate, itemTemplate.transform.parent);
+                    item.SetActive(true);
+                    StartCoroutine(DownLoadIcon(args.Data[i].itemicon, item.transform.Find("ItemIcon").GetComponent<Image>()));
+                    item.transform.Find("ItemName").GetComponent<Text>().text = args.Data[i].itemname;
+                    item.transform.Find("Price").GetComponent<Text>().text = args.Data[i].price.ToString();
+                    var itemIndex = i;
+                    item.transform.Find("Buy").GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        ShowBuy(itemIndex);
+                    });
+                }
+            }
+        });
+    }
+    private bool allowChecking = false;
+    private float waitTime = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (requestPanel.activeSelf)
+        {
+            if (!allowChecking)
+            {
+                return;
+            }
+            if (waitTime > 0)
+            {
+                waitTime -= Time.deltaTime;
+                return;
+            }
+            Grd.GrdManager.CheckItemStatus(request.RequestId, (error, args) =>
+            {
+                if (error == 0)
+                {
+                    if (args.Data != Grd.GrdPurchaseStatus.NewRequest && args.Data != Grd.GrdPurchaseStatus.Pending)
+                    {
+                        requestPanel.transform.GetChild(0).Find("RequestStatus").GetComponent<Text>().text = args.Data.ToString();
+                        allowChecking = false;
+                    }
+                    else
+                    {
+                        waitTime = 2;//wait 2s
+                        allowChecking = true;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Assets/GrdIAP/GrdManager.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Grd
+{
+    internal delegate void GrdNetworkEventHandler(string data);
+    public enum GrdNet
+    {
+        Main, Test
+    }
+    public class GrdManager
+    {
+        public static void Init(string appId, string secret, GrdNet net)
+        {
+            if (handler == null)
+            {
+                GameObject g = new GameObject();
+                g.name = "GrdManager";
+                handler = g.AddComponent<GrdHandler>();
+            }
+            handler.Init(appId, secret, net == GrdNet.Main ? apiMainUrl : apiTestUrl);
+        }
+        private const string apiTestUrl = "https://test.gamereward.io/appapi/";
+        private const string apiMainUrl = "https://gamereward.io/appapi/";
+        private static GrdHandler handler;
+        public static long GetEpochTime()
+        {
+
+            System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+            return (long)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+        }
+        public static string Md5Sum(string strToEncrypt)
+        {
+            System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
+            byte[] bytes = ue.GetBytes(strToEncrypt);
+
+            // encrypt bytes
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(bytes);
+            // Convert the encrypted bytes back to a string (base 16)
+            string hashString = "";
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+            }
+
+            return hashString.PadLeft(32, '0');
+        }
+        private static void GetImageSize(byte[] imageData, out int width, out int height)
+        {
+            width = ReadInt(imageData, 3 + 15);
+            height = ReadInt(imageData, 3 + 15 + 2 + 2);
+        }
+        private static int ReadInt(byte[] imageData, int offset)
+        {
+            return (imageData[offset] << 8) | imageData[offset + 1];
+        }
+        private static Dictionary<string, object> GetObjectData(string data)
+        {
+            Dictionary<string, object> result = null;
+            try
+            {
+                result = (Dictionary<string, object>)MiniJSON.Json.Deserialize(data);
+            }
+            catch
+            {
+            }
+            if (result == null)
+            {
+                result = new Dictionary<string, object>();
+                result.Add("error", 100);
+                result.Add("message", data);
+            }
+            return result;
+        }
+        /// <summary>
+        /// Request for buy an item.
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void RequestBuyItem(string itemid, int quantity, GrdEventHandler<GrdPurchaseRequest> callback)
+        {
+            Dictionary<string, string> pars = new Dictionary<string, string>();
+            pars["itemid"] = itemid;
+            pars["quantity"] = quantity.ToString();
+            handler.Post((data) =>
+            {
+                Dictionary<string, object> result = GetObjectData(data);
+                if (callback != null)
+                {
+                    int error = int.Parse(result["error"].ToString());
+                    GrdEventArgs<GrdPurchaseRequest> args = null;
+                    if (error == 0)
+                    {
+                        GrdPurchaseRequest request = new GrdPurchaseRequest();
+                        request.RequestId = result["requestid"].ToString();
+                        args = new GrdEventArgs<GrdPurchaseRequest>(error, "", data, request);
+                    }
+                    else
+                    {
+                        args = new GrdEventArgs<GrdPurchaseRequest>(error, result["message"].ToString(), data, null);
+                    }
+                    callback(error, args);
+                }
+            }, "requestpurchase", pars);
+        }
+        public static void CheckItemStatus(string requestid, GrdEventHandler<GrdPurchaseStatus> callback)
+        {
+            Dictionary<string, string> pars = new Dictionary<string, string>();
+            pars["requestid"] = requestid;
+            handler.Post((data) =>
+            {
+                Dictionary<string, object> result = GetObjectData(data);
+                if (callback != null)
+                {
+                    int error = int.Parse(result["error"].ToString());
+                    string msg = "";
+                    GrdPurchaseStatus status = GrdPurchaseStatus.NewRequest;
+                    if (error != 0)
+                    {
+                        msg = result["message"].ToString();
+                    }
+                    else
+                    {
+                        status = (GrdPurchaseStatus)int.Parse(result["status"].ToString());
+                    }
+                    GrdEventArgs<GrdPurchaseStatus> args = new GrdEventArgs<GrdPurchaseStatus>(error, msg, data, status);
+                    callback(error, args);
+                }
+            }, "checkpurchasestatus", pars);
+        }
+        /// <summary>
+        /// Get the qrcode from text
+        /// </summary>
+        /// <param name="text">The text to encode to QR code</param>
+        /// <param name="callback">Call when server response QR code.</param>
+        public static void GetRequestPurchaseQRCode(string requestid, GrdEventHandler<Texture2D> callback)
+        {
+            GetQRCode("pay:" + requestid, callback);
+        }
+        /// <summary>
+        /// Get the qrcode from text
+        /// </summary>
+        /// <param name="text">The text to encode to QR code</param>
+        /// <param name="callback">Call when server response QR code.</param>
+        public static void GetQRCode(string text, GrdEventHandler<Texture2D> callback)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("text", text);
+            dic.Add("type", "1");
+            handler.Post((data) =>
+            {
+                if (callback != null)
+                {
+
+                    Dictionary<string, object> result = GetObjectData((string)data);
+                    Texture2D texture = null;
+                    int error = int.Parse(result["error"].ToString());
+                    string message = text;
+                    if (error == 0)
+                    {
+                        string qrcode = result["qrcode"].ToString();
+                        if (qrcode.Length > 0)
+                        {
+                            qrcode = qrcode.Substring("data:image/image/png;base64,".Length);
+                        }
+                        try
+                        {
+                            texture = GetTexture(qrcode);
+                        }
+                        catch
+                        {
+                            error = 1;
+                        }
+                    }
+                    else
+                    {
+                        text = result["message"].ToString();
+                    }
+                    GrdEventArgs<Texture2D> args = new GrdEventArgs<Texture2D>(error, data, text, texture);
+                    callback(error, args);
+                }
+            }, "qrcode", dic);
+        }
+        private static Texture2D GetTexture(string responseText)
+        {
+
+            Texture2D texture = null;
+            byte[] array = System.Convert.FromBase64String(responseText);
+            int width, height;
+            GetImageSize(array, out width, out height);
+            texture = new Texture2D(width, height, TextureFormat.ARGB32, false, true);
+            texture.hideFlags = HideFlags.HideAndDontSave;
+            texture.filterMode = FilterMode.Point;
+            texture.LoadImage(array);
+            return texture;
+        }
+        public static void GetItemsInfo(string[] itemCodes, GrdEventHandler<GrdItem[]> callback)
+        {
+            Dictionary<string, string> pars = new Dictionary<string, string>();
+            pars["items"] = string.Join(",", itemCodes);
+            handler.Post((data) =>
+            {
+                Dictionary<string, object> result = GetObjectData(data);
+                if (callback != null)
+                {
+                    int error = int.Parse(result["error"].ToString());
+                    string msg = "";
+                    List<GrdItem> items = new List<GrdItem>();
+                    if (error == 0)
+                    {
+                        List<object> ls = (List<object>)result["items"];
+                        for (int i = 0; i < ls.Count; i++)
+                        {
+                            Dictionary<string, object> dic = (Dictionary<string, object>)ls[i];
+                            GrdItem item = new GrdItem();
+                            item.itemcode = dic["itemcode"].ToString();
+                            item.itemname = dic["itemname"].ToString();
+                            item.price = decimal.Parse(dic["price"].ToString());
+                            item.itemicon = dic["itemicon"].ToString();
+                            items.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        msg = result["message"].ToString();
+                    }
+                    GrdEventArgs<GrdItem[]> args = new GrdEventArgs<GrdItem[]>(error, msg, data, items.ToArray());
+                    callback(error, args);
+                }
+            }, "getappitems", pars);
+        }
+    }
+    /// <summary>
+    /// Delegate use to handle event when gamereward server response the result
+    /// </summary>
+    /// <param name="error">Error value: 0 if no error, else there was an error while call</param>
+    /// <param name="args">args contains properties: Text is the text data field,RawData is the json data response from server</param>
+    public delegate void GrdEventHandler<T>(int error, GrdEventArgs<T> args);
+
+    public class GrdEventArgs<T>
+    {
+        public GrdEventArgs(int error, string errorMessage, string rawData, T data)
+        {
+            this.data = data;
+            this.RawData = rawData;
+            this.error = error;
+            this.errorMessage = errorMessage;
+        }
+        private int error;
+        private T data;
+        private string errorMessage;
+        public string RawData { get; private set; }
+        public T Data { get { return data; } }
+        public string ErrorMessage { get { if (error == 0) { return ""; } else { return errorMessage; } } }
+    }
+    public enum GrdPurchaseStatus
+    {
+        NewRequest = 0, Success = 1, Pending = 2, Error = 3
+    }
+    public class GrdItem
+    {
+        public string itemcode;
+        public string itemname;
+        public string itemicon;
+        public decimal price;
+    }
+    public class GrdPurchaseRequest
+    {
+        public string RequestId { get; set; }
+        public GrdPurchaseStatus Status { get; set; }
+    }
+}
